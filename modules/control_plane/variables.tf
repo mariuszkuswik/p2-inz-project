68a1f18319086a2c4efb686b386ccef68a1056c1
Mariusz Kuswik <mariusz.kuswik1@gmail.com>
2024-01-22 12:40:40 +0100

    control_node dziala

---
+++ modules/control_plane/variables.tf
@@ -0,0 +1,9 @@
+variable "control_plane_disk_path" {
+  type    = string
+  default = "/home/mariusz/p2/terra_kvm/meta/rhel/rhel-7_9-control-plane.qcow2"
+}
+
+variable "control_plane_name" {
+  type    = string
+  default = "control_plane"
+}

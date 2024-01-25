07b85a783e53c5ffad4b521e573177acdce92684
Mariusz Kuswik <mariusz.kuswik1@gmail.com>
2024-01-25 23:06:37 +0100

    tests

--- Terraform/modules/control_plane/variables.tf
+++ Terraform/modules/control_plane/variables.tf
@@ -3,12 +3,9 @@ variable "node_disk_path" {
   default = "/home/mariusz/p2/terra_kvm/meta/rhel/rhel-8-control-plane.qcow2"
 }
 
-variable "node_name" {
-  type    = string
-  default = "control_plane"
-}
-
 variable "repo_path" {
   type    = string
   default = "/home/mariusz/p2/terra_kvm/meta/repo/repos.iso"
 }
+
+

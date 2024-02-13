ab00248d7cc1e2d4894f9d92d10874575912c452
Mariusz Kuswik <mariusz.kuswik1@gmail.com>
2024-02-13 23:04:58 +0100

    virsh script

--- virsh_destroy.sh
+++ virsh_destroy.sh
@@ -1,6 +1,12 @@
 # Virsh domains
 virsh shutdown control_plane
 virsh undefine control_plane
+# Node1
+virsh shutdown node1 
+virsh undefine node1
+# Node2
+virsh shutdown node2
+virsh undefine node2
 # Volumes
 virsh vol-delete control_plane.iso default
 virsh vol-delete node1.iso default

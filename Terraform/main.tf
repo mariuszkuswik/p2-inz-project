ac410d7e2d78cc97d453a90d89784d8adddfe5f9
Mariusz Kuswik <mariusz.kuswik1@gmail.com>
2024-02-15 22:05:38 +0100

    disks works

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -13,20 +13,10 @@ module "control_plane" {
 
 module "node" {
   # Number of instances!
-  num_instances     = 2
+  num_instances     = 1
   source            = "./modules/node"
-  # hostname          = "node1" 
   node_disk_path    = "/home/mariusz/p2-meta/rhel/rhel-8-node-sample.qcow2" 
   network_name      = "internal"
-  # addresses         = "[192.168.2.101]"
   autostart         = false
   storage_pool      = "default"
 }
-
-
-# module "node2" {
-#   source            = "./modules/node"
-#   hostname          = "node2" 
-#   node_disk_path    = "/home/mariusz/p2-meta/rhel/rhel-8-node2.qcow2" 
-# }
-

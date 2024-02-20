423ee2aff4bf70cbac02844be4069b6dd7a84b1b
mariusz <mariusz@DESKTOP-JDUHK2L.localdomain>
2024-02-20 22:50:01 +0100

    nat

--- Terraform/modules/network/main.tf
+++ Terraform/modules/network/main.tf
@@ -4,15 +4,9 @@ variable "network_name" {
 
 resource "libvirt_network" "internal" {
   name = var.network_name
-  mode = "bridge"
+  mode = "nat"
 
-  bridge {
-    name           = "br0"
-    interface_type = "ethernet"
-  }
-
-
-  addresses = ["192.168.1.0/24"]
+  addresses = ["192.168.2.0/24"]
 
   dhcp {
     enabled = true

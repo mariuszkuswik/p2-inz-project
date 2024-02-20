0716800694f034fb1d80db0fad09b535a1a263c9
mariusz <mariusz@DESKTOP-JDUHK2L.localdomain>
2024-02-20 22:43:57 +0100

    bridge2

--- Terraform/modules/network/main.tf
+++ Terraform/modules/network/main.tf
@@ -1,19 +1,20 @@
-variable "network_name" {
-  type = string
-}
-
 resource "libvirt_network" "internal" {
   name = var.network_name
-  mode = "nat"
+  mode = "bridge"
 
-  addresses = ["192.168.2.0/24"]
+  bridge {
+    name           = "br0"
+    interface_type = "ethernet"
+  }
+
+  addresses = ["192.168.0.0/24"]  # Adjust the subnet to match your network
 
   dhcp {
     enabled = true
   }
-  
-  dns { 
-    enabled = true
+
+  dns {
+    enabled    = true
     local_only = false
   }
 }

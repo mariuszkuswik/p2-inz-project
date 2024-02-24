4586e80e11ad735f897186dae4c7ae1b15dcd9ac
mariusz <mariusz@DESKTOP-JDUHK2L.localdomain>
2024-02-24 18:47:11 +0100

    docker test

---
+++ RunAll/main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+class Templates:
+    def __init__(self):
+        pass
+        
+    def test(self,age):
+        self.age = test
+        
+        
+        
+
+oTest=Test()

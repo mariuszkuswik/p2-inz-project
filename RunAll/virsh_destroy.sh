7ebe9dfa77dae11b84df1d3a7a2083ecd9eb4881
Mariusz Kuswik <mariusz.kuswik1@gmail.com>
2024-03-02 12:44:53 +0100

    virsh destroy corrected

--- RunAll/virsh_destroy.sh
+++ RunAll/virsh_destroy.sh
@@ -1,6 +1,6 @@
 # Virsh domains
-virsh shutdown control_plane
-virsh undefine control_plane
+virsh shutdown control-plane
+virsh undefine control-plane
 # Node1
 virsh shutdown node1 
 virsh undefine node1
@@ -8,9 +8,9 @@ virsh undefine node1
 virsh shutdown node2
 virsh undefine node2
 # Volumes
-virsh vol-delete control_plane.iso default
+virsh vol-delete control-plane.iso default
 virsh vol-delete node1.iso default
 virsh vol-delete node2.iso default
 # Network
-virsh net-undefine internal
-virsh net-destroy internal
+virsh net-undefine bridgenet 
+virsh net-destroy bridgenet
